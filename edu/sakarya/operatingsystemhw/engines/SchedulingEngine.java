db29bb725d6d9cfd74b22b237b1fb488f3398926
oSoloTurk <hakki.ceylan2356@gmail.com>
2022-12-18 01:35:19 +0300

    base of scheduling engine

---
+++ edu/sakarya/operatingsystemhw/engines/SchedulingEngine.java
@@ -0,0 +1,60 @@
+package edu.sakarya.operatingsystemhw.engines;
+
+import edu.sakarya.operatingsystemhw.enums.States;
+import edu.sakarya.operatingsystemhw.interfaces.ITask;
+import edu.sakarya.operatingsystemhw.managers.QueueManager;
+import edu.sakarya.operatingsystemhw.models.Task;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SchedulingEngine {
+    private int runtimeClock;
+    private QueueManager queueManager;
+    private static SchedulingEngine instance;
+
+    private SchedulingEngine() {
+        this.runtimeClock = 0;
+        this.queueManager = QueueManager.getInstance();
+    }
+
+    public void run(){
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                runtimeClock++;
+                Task task = queueManager.getNextTask();
+                if(task != null){
+                    switch(task.getState()){
+                        case READY:
+                        case STOPPED:
+                        case WAITING:
+                            task.setState(States.RUNNING);
+                            task.onStateChanged();
+                        default:
+                            task.burn();
+                            if(task.getBurnTime() >= task.getProcessTime()){
+                                task.setState(States.STOPPED);
+                                task.onStateChanged();
+                            }
+                    }
+                }
+            }
+        };
+        timer.scheduleAtFixedRate(timerTask, 0, 1000);
+    }
+
+    public static SchedulingEngine getInstance() {
+        if (instance == null) {
+            instance = new SchedulingEngine();
+        }
+        return instance;
+    }
+
+    public int getRuntimeClock() {
+        return runtimeClock;
+    }
+
+}
