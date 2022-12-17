630067d56c624ee983392463e3e755a8a39f83a0
oSoloTurk <hakki.ceylan2356@gmail.com>
2022-12-18 01:38:09 +0300

    engine starts

--- edu/sakarya/operatingsystemhw/Main.java
+++ edu/sakarya/operatingsystemhw/Main.java
@@ -1,14 +1,29 @@
 package edu.sakarya.operatingsystemhw;
 
-import edu.sakarya.operatingsystemhw.enums.JobQueues;
-import edu.sakarya.operatingsystemhw.models.JobQueue;
+import edu.sakarya.operatingsystemhw.engines.SchedulingEngine;
+import edu.sakarya.operatingsystemhw.managers.JobDispatcher;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		JobQueue queue = new JobQueue();
-		queue.setQueueEngine(JobQueues.FIFO_QUEUE_ENGINE);
+	public static void main(String[] args) throws FileNotFoundException {
+		if(args.length == 0) {
+			System.out.println("Please enter the file path as a parameter.");
+			return;
+		}
 
+		File file = new File(args[0]);
+		if(!file.exists()) {
+			System.out.println("File not found.");
+			return;
+		}
+		SchedulingEngine schedulingEngine = SchedulingEngine.getInstance();
+		JobDispatcher dispatcher = new JobDispatcher(file);
+
+		schedulingEngine.run();
+		dispatcher.run();
 	}
 	
 }
